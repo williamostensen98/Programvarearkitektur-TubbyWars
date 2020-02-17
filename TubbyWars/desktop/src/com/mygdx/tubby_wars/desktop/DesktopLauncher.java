9f2e997f57a707e14263d0a75d6f89e1d70cad11
William Østensen <william-98-@hotmail.com>
2020-02-17 16:04:53 +0100

    Initial commit

---
+++ TubbyWars/desktop/src/com/mygdx/tubby_wars/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.tubby_wars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.tubby_wars.TubbyWars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TubbyWars(), config);
+	}
+}
