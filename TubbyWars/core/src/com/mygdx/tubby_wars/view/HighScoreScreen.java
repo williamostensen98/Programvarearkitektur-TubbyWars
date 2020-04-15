f7b07e05a7939602aaddd82e6b1fc2464546fb12
sunnivbl <sunnivbl@stud.ntnu.no>
2020-04-15 11:45:42 +0200

    Issue #10 Implementing table for highscorelist

--- TubbyWars/core/src/com/mygdx/tubby_wars/view/HighScoreScreen.java
+++ TubbyWars/core/src/com/mygdx/tubby_wars/view/HighScoreScreen.java
@@ -4,13 +4,18 @@ import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -26,17 +31,21 @@ public class HighScoreScreen extends ScreenAdapter implements ScreenInterface{
     private Texture backB;
     private Texture background;
 
-    //Initiializing the background music
+    //Initializing the background music
     private Music music;
 
     private Sprite sprite;
     private SpriteBatch sb;
     private Stage stage;
 
+
+    Table table = new Table();
+
     public HighScoreScreen(TubbyWars game, Engine engine){
         super();
         this.game = game;
         this.engine = engine;
+
 
         background = Assets.getTexture(Assets.mainBackground);
         backB = Assets.getTexture(Assets.backButton);
@@ -51,8 +60,39 @@ public class HighScoreScreen extends ScreenAdapter implements ScreenInterface{
     }
 
     public void create(){
+
         stage = new Stage(new ScreenViewport());
         sb = new SpriteBatch();
+
+        TextField.TextFieldStyle style = new TextField.TextFieldStyle();
+        style.font = new BitmapFont();
+        style.fontColor = Color.BLACK;
+
+        TextField field = new TextField("", style);
+        field.setText("Test");
+        field.setWidth(150);
+
+        Table menuTable = new Table(); // Table containing the buttons on the screen
+        menuTable.add(field);
+        menuTable.getCell(field).height(300).width(413);
+        menuTable.row();
+        menuTable.add(field);
+        menuTable.getCell(field).height(300).width(413);
+        menuTable.row();
+        /*menuTable.add(setBtn).pad(10);
+
+        menuTable.row();
+        menuTable.add(tutBtn);
+        menuTable.getCell(tutBtn).height(buttonHeight).width(413);
+        menuTable.setFillParent(true);
+        menuTable.moveBy(0,-240);
+        menuTable.row();
+        menuTable.add(signInBtn).pad(10);
+        menuTable.getCell(signInBtn).height(buttonHeight).width(413);
+        menuTable.setFillParent(true);*/
+
+        //stage.addActor(menuTable);
+        stage.addActor(field);
 
         Gdx.input.setInputProcessor(stage);
 
@@ -94,6 +134,8 @@ public class HighScoreScreen extends ScreenAdapter implements ScreenInterface{
     @Override
     public void render(float dt){
         update(dt);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         draw();
     }
 
